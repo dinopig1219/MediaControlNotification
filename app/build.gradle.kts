1884c06c4f6e0de9b3848b0829ee77fe9b9e17a2
DinoPig <107846920+dinopig1219@users.noreply.github.com>
2026-07-27 18:01:33 +0800

    Add miuix blur dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,10 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material:material-icons-core")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("top.yukonga.miuix.kmp:miuix-ui-android:0.9.3")
     implementation("top.yukonga.miuix.kmp:miuix-preference-android:0.9.3")
-    implementation("androidx.compose.material:material-icons-core")
+    implementation("top.yukonga.miuix.kmp:miuix-blur-android:0.9.3")
 }

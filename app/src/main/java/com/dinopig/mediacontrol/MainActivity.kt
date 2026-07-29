922a77195fbf41f302c6c1ae46c18d2999dbbeef
DinoPig <107846920+dinopig1219@users.noreply.github.com>
2026-07-29 15:20:38 +0800

    Adjust

--- app/src/main/java/com/dinopig/mediacontrol/MainActivity.kt
+++ app/src/main/java/com/dinopig/mediacontrol/MainActivity.kt
@@ -451,7 +451,7 @@ private fun AboutScreen(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(logoHeightDp + 52.dp + 40.dp + 126.dp)
+                    .height(logoHeightDp + 52.dp + 40.dp + 110.dp)
                     .onSizeChanged { size -> onLogoSpacerHeightChanged(size.height) }
             )
         }
